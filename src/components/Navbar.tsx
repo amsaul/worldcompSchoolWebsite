8fac95a9b74b104f4d5866b4bd4fad7a5c963a67
amsaul <amsaulbosire@gmail.com>
2025-10-05 15:20:03 +0300

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center h-20">
           <Link to="/" className="flex items-center space-x-3 group">
             <div className=" p-2 rounded-lg group-hover:scale-110 transition-transform">
-              <img src="src/images/logo.png" alt="" width={46}/>
+              <img src="https://i.postimg.cc/3r2DkSrM/logo.png" alt="" width={46}/>
             </div>
             <div>
               <h1 className="text-2xl font-bold text-gray-800">Worldcomp Lenana Academy</h1>
