750c512442fdd3a8063ae910e13433c7573f382f
amsaul <amsaulbosire@gmail.com>
2025-10-05 11:44:52 +0300

    added a coming soon page and styled it a bit

---
+++ src/pages/ComingSoon.tsx
@@ -0,0 +1,12 @@
+const ComingSoon = () =>{
+    return(
+        <>
+            <div className="comingSoon">
+                <h2>Ooops!</h2>
+                <p>Page coming soon, keep checking</p>
+            </div>
+        </>
+    )
+}
+
+export default ComingSoon
