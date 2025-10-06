46f7955622de15f01d424311509f38c4ce4a8cff
amsaul <amsaulbosire@gmail.com>
2025-10-07 00:09:58 +0300

    teacher login svg

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -2,6 +2,7 @@ const ComingSoon = () =>{
     return(
         <>
             <div className="comingSoon">
+                <img src="https://front-office.statics.backmarket.com/e63c5a948de71997af50d19ed84a6dc2de8356f1/img/not-found.svg" alt="" />
                 <h2>Ooops!</h2>
                 <p>Page coming soon, keep checking</p>
             </div>
